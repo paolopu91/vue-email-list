957cae1eda020eb0e1a9d55f839beee4ee92aa2a
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-17 16:02:37 +0200

    added stamp of one email

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
+axios
+.get("https://flynn.boolean.careers/exercises/api/random/mail")
+.then((axiosEmail) => {
+    // stamp in console result of axiosEmail
+    console.log(axiosEmail)
+
+    
+});
+
+
+
 new Vue({
-    el:"#app",
+    el:"#demo",
     data:{
+        emailRandom: null,
+    },
+
+    methods:{
+        getRandomEmail(){
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((axiosEmail) => {
+                    // stamp in console result of axiosEmail
+                    // console.log(this.getRandomEmail)
+
+                    // stamp result in html with key {{emailRandom}}
+                    this.emailRandom = axiosEmail.data.response
+            });
+        },
         
     }
-})
+});
+
