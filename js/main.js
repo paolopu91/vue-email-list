395ec55a15d6054c295a0c1303a41b1cdeabb6a2
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-19 22:09:14 +0200

    added changes to the finished exercise

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ new Vue({
 
         getTenEmailRandom(){
             for(let i=0 ; i<10 ;i++){
+                
 
                axios
                 .get("https://flynn.boolean.careers/exercises/api/random/mail")
